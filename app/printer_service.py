32b4e62becb05b361e14d7f0e4bd37b8c7a0c17a
Marek Brož <marek.broz@student.sps-cl.cz>
2024-12-06 23:58:31 +0100

    completing /app for services

---
+++ app/printer_service.py
@@ -0,0 +1,20 @@
+from brother_ql.raster import BrotherQLRaster
+from brother_ql.backends.helpers import send_to_printer
+from PIL import Image
+
+def print_label(code):
+    printer = "usb://0x04f9:0x2015/000M6Z401370" #identifikator pro tiskarnu
+    model = "QL-800"
+    #typ stitku
+    label = "62"
+    image_path = f"./archive/printed/{code}.png"
+
+    #prevod pdf na png
+    image = Image.new('1', (696, 292), 255)
+    image.save(image_path)
+
+    qlr = BrotherQLRaster(model)
+    qlr.exception_on_warning = True
+    qlr.convert(image_path, label, rotate="auto")
+
+    send_to_printer(qlr.data, printer, backend="pyusb")
