8f8c9d70875bea5ead9c13e5f366412ad7ae36ce
Marek Brož <marek.broz@student.sps-cl.cz>
2024-12-31 02:41:14 +0100

    update gitignore and printer-service

--- printer-service.py
+++ printer-service.py
@@ -5,24 +5,19 @@ from PIL import Image
 
 def print_label_with_image(image_path, printer_model, usb_path, label_type='62'):
     try:
-        # Load PNG image
         image = Image.open(image_path)
-        image = image.convert('1')  # Convert to black and white
+        image = image.convert('1')
 
-        # Resize to fit the label size
         target_width = 696
         if image.width != target_width:
             scale_factor = target_width / image.width
             target_height = int(image.height * scale_factor)
             image = image.resize((target_width, target_height), Image.Resampling.LANCZOS)
 
-        # Initialize the printer
         qlr = BrotherQLRaster(printer_model)
 
-        # Convert image to printing instructions
         instructions = convert(qlr, [image], label=label_type, rotate='0')
 
-        # Send to printer
         send(instructions, usb_path)
         print(f"Printing '{image_path}' completed")
     except Exception as e:
